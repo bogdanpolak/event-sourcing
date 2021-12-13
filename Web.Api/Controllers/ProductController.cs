acb0b442cfa3016864d1049c776667bca187d4c0
Bogdan Polak <bogdanpol@gmail.com>
2021-12-13 21:32:58 +0100

    Added EFCore and ProductDbContext

---
+++ Web.Api/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.EventSourcing;
+
+namespace Web.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _dbContext;
+
+        public ProductController(
+            ProductDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        [HttpGet]
+        public IEnumerable<Product> GetProduct()
+        {
+            var number = _dbContext.Products.Count()+1;
+            _dbContext.Products.Add(new Product
+            {
+                Sku = CreateNewSku(number),
+                Received = 100,
+                Shipped = 45
+            });
+            _dbContext.Products.Add(new Product
+            {
+                Sku = CreateNewSku(number),
+                Received = 250,
+                Shipped = 190
+            });
+            _dbContext.SaveChanges();
+            
+            return _dbContext.Products.ToList();
+        }
+
+        private string CreateNewSku(int number)
+        {
+            var random = new Random();
+            return $"INV-{10000 + random.Next(89999)}-{number}";
+        }
+    }
+}
--- Web.Api/Startup.cs
+++ Web.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Web.Api.EventSourcing;
 
 namespace Web.Api
 {
@@ -27,7 +28,11 @@ namespace Web.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web.Api", Version = "v1" }); });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web.Api", Version = "v1" });
+            });
+            services.AddDbContext<ProductDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
