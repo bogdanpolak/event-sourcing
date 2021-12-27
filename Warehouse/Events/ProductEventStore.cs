17ae8fef09bc9cea5a183d459d4fb284459c7e69
Bogdan Polak <bogdanpol@gmail.com>
2021-12-27 21:07:00 +0100

    Done ProductEventStore - Build_Scenario1

---
+++ Warehouse/Events/ProductEventStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Warehouse.Utils;
+
+namespace Warehouse.Events
+{
+    public static class ProductEventStore
+    {
+        public static IEnumerable<IEvent> Build_Scenario1()
+        {
+            var events = new IEvent[]
+            {
+                new ProductAdjusted(Sku.Laptop, 3, 7.May(2021)),
+                new ProductAdjusted(Sku.Monitor,5, 14.May(2021)),
+                new ProductAdjusted(Sku.DockingStation, 4, 14.May(2021)),
+                new ProductReceived(Sku.Laptop, 5, 10.May(2021)),
+                new ProductShipped(Sku.Laptop, 1, 11.May(2021)),
+                new ProductReceived(Sku.Monitor,10, 14.May(2021)),
+                new ProductReceived(Sku.DockingStation, 5, 14.May(2021)),
+                new ProductShipped(Sku.Laptop,3, 17.May(2021)),
+                new ProductShipped(Sku.DockingStation, 3, 17.May(2021)),
+                new ProductShipped(Sku.Monitor, 3, 17.May(2021)),
+                new ProductReceived(Sku.Laptop, 5, 20.May(2021)),
+                new ProductShipped(Sku.Laptop, 1, 21.May(2021)),
+            };
+            return events;
+        }
+
+        private static class Sku
+        {
+            public static string Laptop { get; } = CreateNewSku("C-", "-laptop");
+            public static string DockingStation { get; } = CreateNewSku("C-", "-docking");
+            public static string Monitor { get; } = CreateNewSku("C-", "-monitor");
+
+            private static string CreateNewSku(string prefix, string suffix)
+            {
+                var random = new Random();
+                return $"{prefix}{100000 + random.Next(899999)}{suffix}";
+            }
+        }
+    }
+}
--- Web.Api/Controllers/ProductController.cs
+++ Web.Api/Controllers/ProductController.cs
@@ -23,57 +23,13 @@ namespace Web.Api.Controllers
         [HttpGet]
         public IActionResult GetProduct()
         {
-            var events = new IEvent[]
-            {
-                new ProductAdjusted(Sku.Laptop, 3, 7.May(2021)),
-                new ProductAdjusted(Sku.Monitor,5, 14.May(2021)),
-                new ProductAdjusted(Sku.DockingStation, 4, 14.May(2021)),
-                new ProductReceived(Sku.Laptop, 5, 10.May(2021)),
-                new ProductShipped(Sku.Laptop, 1, 11.May(2021)),
-                new ProductReceived(Sku.Monitor,10, 14.May(2021)),
-                new ProductReceived(Sku.DockingStation, 5, 14.May(2021)),
-                new ProductShipped(Sku.Laptop,3, 17.May(2021)),
-                new ProductShipped(Sku.DockingStation, 3, 17.May(2021)),
-                new ProductShipped(Sku.Monitor, 3, 17.May(2021)),
-                new ProductReceived(Sku.Laptop, 5, 20.May(2021)),
-                new ProductShipped(Sku.Laptop, 1, 21.May(2021)),
-            };
-
+            var events = ProductEventStore.Build_Scenario1();
+            
             ProductFlowProjection.Build(events, _dbContext);
 
             var products = _dbContext.ProductsFlows.ToArray();
 
             return Ok(products);
         }
-
-        private static class Sku
-        {
-            public static string Laptop { get; } = CreateNewSku("C-", "-laptop");
-            public static string DockingStation { get; } = CreateNewSku("C-", "-docking");
-            public static string Monitor { get; } = CreateNewSku("C-", "-monitor");
-
-            private static string CreateNewSku(string prefix, string suffix)
-            {
-                var random = new Random();
-                return $"{prefix}{100000 + random.Next(899999)}{suffix}";
-            }
-
-        }
-    }
-
-    internal static class IntExtension
-    {
-        public static DateTime Jan(this int day, int year) => new DateTime(year, 1, day);
-        public static DateTime Feb(this int day, int year) => new DateTime(year, 2, day);
-        public static DateTime Mar(this int day, int year) => new DateTime(year, 3, day);
-        public static DateTime Apr(this int day, int year) => new DateTime(year, 4, day);
-        public static DateTime May(this int day, int year) => new DateTime(year, 5, day);
-        public static DateTime Jun(this int day, int year) => new DateTime(year, 6, day);
-        public static DateTime Jul(this int day, int year) => new DateTime(year, 7, day);
-        public static DateTime Aug(this int day, int year) => new DateTime(year, 8, day);
-        public static DateTime Sep(this int day, int year) => new DateTime(year, 9, day);
-        public static DateTime Oct(this int day, int year) => new DateTime(year, 10, day);
-        public static DateTime Nov(this int day, int year) => new DateTime(year, 11, day);
-        public static DateTime Dec(this int day, int year) => new DateTime(year, 12, day);
     }
 }
